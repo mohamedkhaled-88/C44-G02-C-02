91365af390087f405f03e04bb3ff75ab123cce5a
Mohamed Khaled <mk5710744@gmail.com>
2025-06-23 18:44:09 +0300

    Add project files.

---
+++ Session2/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Session2;//Main namespace for the project
+class Program
+{
+    //Class, Struct, Enum, and Interface definitions can go here
+    #region XMLComments
+    /// <summary>
+    /// This method sums two integers and returns the result.
+    /// </summary>
+    /// <param name="num1">this is the first num</param>
+    /// <param name="num2">This is the secend num</param>
+    /// <returns>The result will be the sum of two num1+num2</returns>
+    //static int Sum(int num1, int num2)
+    //{
+    //    return num1 + num2;
+    //}
+    /// <summary>
+    /// This method prints an integer to the console.
+    /// </summary>
+    /// <param name="x">thid id integer num</param>
+    //static void Print (int x)
+    //{
+    //    Console.WriteLine(x);
+    //}
+    #endregion 
+
+    static void Main(string[] args) //Entry point for the program
+    {
+        Console.WriteLine("Hello, World!");
+        Console.ReadLine(); //Waits for user input before closing the console window
+                                 //Console :Class is used to output text to the console
+                                 //WriteLine: method prints the text followed by a new line
+#region Comments and Regions
+// This is a single-line comment
+//int x== 5;
+//int y = 5;
+/* 
+ * This is a multi-line comment
+ * int x== 5;
+ * int y = 5;
+ */
+#endregion
+
+#region Qusation 01
+//int x = 10;
+//int y = 20;
+//Console.WriteLine(x+y);
+//Console.WriteLine("Hello");
+#endregion 
+
+
+    }
+}
+
+
